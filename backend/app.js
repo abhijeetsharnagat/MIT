b9fe7d8e40cf7670bd2c914636a8854a6404aea2
abhijeetsharnagat <108009757+abhijeetsharnagat@users.noreply.github.com>
2023-11-06 00:38:07 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['http://localhost:3000',],
+  origin: ['https://mit-api-abhijeet-sharnagats-projects.vercel.app',],
   credentials: true
 }));
 
